92f501cd2971509c2234a80abbc3925c3d245470
Logan Collins <loganscollins@gmail.com>
2012-07-08 14:29:11 -0700

    Added PXTreeNode

---
+++ Photon/Trees/PXTreeNode.h
@@ -0,0 +1,54 @@
+//
+//  PXTreeNode.h
+//  Photon
+//
+//  Created by Logan Collins on 7/8/12.
+//  Copyright (c) 2012 Sunflower Softworks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+/*!
+ * @protocol PXTreeNode
+ * @abstract Implemented by objects participating in a tree of objects
+ */
+@protocol PXTreeNode <NSObject>
+
+@required
+
+@property (copy, readonly) NSString *title;                 /* The title of the item */
+@property (copy, readonly) NSImage *image;                  /* The image of the item */
+@property (readonly) NSUInteger order;                      /* The order of the item, 0 is lowest */
+@property (copy, readonly) NSSet *children;                 /* The set of child items */
+
+@property (readonly, getter=isSelectable) BOOL selectable;	/* If the item is selectable by the user */
+@property (readonly, getter=isEditable) BOOL editable;		/* If the item is editable by the user */
+@property (readonly, getter=isGroupItem) BOOL groupItem;	/* If the item is a group heading */
+
+@optional
+
+@property (copy, readonly) NSStirng *persistenceString;     /* A unique string representation of the receiver, or nil */
+
+@end
+
+
+/*!
+ * @class PXTreeNode
+ * @abtract A concrete, generic implementation of the PXTreeNode protocol
+ */
+@interface PXTreeNode : NSObject <PXTreeNode>
+
+@property (copy) NSString *title;
+@property (copy) NSImage *image;
+@property NSUInteger order;
+
+@property (copy) NSSet *children;
+- (void)addChild:(id <LCSourceListItem>)child;
+- (void)removeChild:(id <LCSourceListItem>)child;
+
+@property (getter=isSelectable) BOOL selectable;
+@property (getter=isEditable) BOOL editable;
+@property (getter=isGroupItem) BOOL groupItem;
+
+@end
---
+++ Photon/Trees/PXTreeNode.m
@@ -0,0 +1,14 @@
+//
+//  PXTreeNode.m
+//  Photon
+//
+//  Created by Logan Collins on 7/8/12.
+//  Copyright (c) 2012 Sunflower Softworks. All rights reserved.
+//
+
+#import "PXTreeNode.h"
+
+
+@implementation PXTreeNode
+
+@end
