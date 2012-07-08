e43b50cf7e1943d6e60b8c54a51aac84b34cef2c
Logan Collins <loganscollins@gmail.com>
2012-07-08 14:32:42 -0700

    Added PXTreeNode

--- Photon/Trees/PXTreeNode.m
+++ Photon/Trees/PXTreeNode.m
@@ -9,6 +9,59 @@
 #import "PXTreeNode.h"
 
 
-@implementation PXTreeNode
+@implementation PXTreeNode {
+    NSMutableSet *_children;
+}
+
+@synthesize title=_title;
+@synthesize image=_image;
+@synthesize order=_order;
+
+@synthesize selectable=_selectable;
+@synthesize editable=_editable;
+@synthesize groupItem=_groupItem;
+
+- (id)init {
+    self = [super init];
+    if (self) {
+        _children = [NSMutableSet set];
+    }
+    return self;
+}
+
+
+#pragma mark -
+#pragma mark Accessors
+
+- (NSArray *)children {
+    return [_children copy];
+}
+
+- (void)setChildren:(NSSet *)children {
+    if (![children isEqualToSet:newChildren]) {
+		NSSet *objects = [NSSet setWithSet:children];
+		[self willChangeValueForKey:@"children" withSetMutation:NSKeyValueSetSetMutation usingObjects:objects];
+		[children setSet:children];
+		[self didChangeValueForKey:@"children" withSetMutation:NSKeyValueSetSetMutation usingObjects:objects];
+	}
+}
+
+- (void)addChild:(id <LCSourceListItem>)child {
+    if (![children containsObject:child]) {
+		NSSet *objects = [NSSet setWithObject:child];
+		[self willChangeValueForKey:@"children" withSetMutation:NSKeyValueUnionSetMutation usingObjects:objects];
+		[children addObject:child];
+		[self didChangeValueForKey:@"children" withSetMutation:NSKeyValueUnionSetMutation usingObjects:objects];
+	}
+}
+
+- (void)removeChild:(id <LCSourceListItem>)child {
+    if ([children containsObject:child]) {
+		NSSet *objects = [NSSet setWithObject:child];
+		[self willChangeValueForKey:@"children" withSetMutation:NSKeyValueMinusSetMutation usingObjects:objects];
+		[children removeObject:child];
+		[self didChangeValueForKey:@"children" withSetMutation:NSKeyValueMinusSetMutation usingObjects:objects];
+	}
+}
 
 @end
