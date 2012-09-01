3b14488029ccf47ae96daf4b587368abdd407b73
Logan Collins <logan@panic.com>
2012-08-31 17:04:04 -0700

    Updated documentation.

--- Photon/Categories/NSObject+PhotonAdditions.h
+++ Photon/Categories/NSObject+PhotonAdditions.h
@@ -11,24 +11,51 @@
 
 @interface NSObject (PhotonAdditions)
 
-/* @method px_addObserver:forKeyPaths:options:context:
+/*!
+ * @method px_addObserver:forKeyPaths:options:context:
  * @abstract Add a KVO observer for multiple key paths
+ * 
+ * @param observer
+ * The object for which to add observations
+ * 
+ * @param keyPaths
+ * The key paths for which to add observations
+ * 
+ * @param context
+ * The context to use for the observations
  * 
  * @discussion
  * This is a convenience wrapper for adding a KVO observer for multiple key paths simultaneously
  */
 - (void)px_addObserver:(NSObject *)observer forKeyPaths:(NSSet *)keyPaths options:(NSKeyValueObservingOptions)options context:(void *)context;
 
-/* @method px_removeObserver:forKeyPaths:
+/*!
+ * @method px_removeObserver:forKeyPaths:
  * @abstract Remove a KVO observer for multiple key paths
+ *
+ * @param observer
+ * The object for which to remove observations
+ *
+ * @param keyPaths
+ * The key paths for which to remove observations
  * 
  * @discussion
  * This is a convenience wrapper for removing a KVO observer for multiple key paths simultaneously
  */
 - (void)px_removeObserver:(NSObject *)observer forKeyPaths:(NSSet *)keyPaths;
 
-/* @method px_removeObserver:forKeyPaths:context:
+/*!
+ * @method px_removeObserver:forKeyPaths:context:
  * @abstract Remove a KVO observer for multiple key paths
+ * 
+ * @param observer
+ * The object for which to remove observations
+ * 
+ * @param keyPaths
+ * The key paths for which to remove observations
+ * 
+ * @param context
+ * The context used for the observations
  * 
  * @discussion
  * This is a convenience wrapper for removing a KVO observer for multiple key paths simultaneously
