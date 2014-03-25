f19221d5d67868557e658b69478edbfbe4ed6f0b
Logan Collins <logan@panic.com>
2014-03-25 16:54:26 -0700

    Small fix to preferences.

--- Photon/Preferences/PXPreferencesController.m
+++ Photon/Preferences/PXPreferencesController.m
@@ -450,6 +450,7 @@
 
 - (void)pane:(PXPreferencePane *)pane didCommitForClose:(BOOL)didCommit contextInfo:(void *)contextInfo {
     if (didCommit) {
+        [_currentPane viewWillDisappear:YES];
         [_window close];
     }
 }
